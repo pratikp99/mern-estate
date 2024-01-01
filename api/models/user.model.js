a1cf4450839aecbbaa4d54f740dcaad65d39b8e9
Pratik <pratikpattanshetti1007@gmail.com>
2024-01-01 17:19:32 +0530

    create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
-}, {timeStamp:true});
+}, {timeStamps:true});
 
 const User=mongoose.model('User',userSchema)
 

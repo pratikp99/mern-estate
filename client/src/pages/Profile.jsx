f6cdd358ec47339075ef0d833e09173427a465e0
Pratik <pratikpattanshetti1007@gmail.com>
2024-01-05 01:30:45 +0530

    add signout functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import {useRef, useState,useEffect} from 'react';
 import storage from 'redux-persist/lib/storage';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';  
-import { deleteUserFailure, deleteUserStart, deleteUserSuccess, updateUserFailure,updateUserStart,updateUserSuccess } from '../redux/user/userSlice';
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, updateUserFailure,updateUserStart,updateUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -89,6 +89,20 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message))
     }
   }
+  const handleSignOut=async ()=>{
+    try {
+      dispatch(signOutUserStart());
+      const res=await fetch('/api/auth/signout');
+      const data=await res.json();
+      if(data.success===false){
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data.message));
+    } catch (error) {
+      
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'><h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
     <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
@@ -111,8 +125,8 @@ export default function Profile() {
       <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'> {loading?'Loading...':'Update'}</button>
     </form>
     <div className='flex justify-between mt-5'>
-      <span onClick={handleDeleteUser} className='text-red-700 curser-pointer'>Delete Account</span>
-      <span className='text-red-700 curser-pointer'>Sign Out</span>
+      <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
+      <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
     </div>
     <p className='text-red-700 mt-5'>{error?error:''}</p>
     <p className='text-green-700 mt-5'>{updateSuccess?'User Is Updated Successfully':''}</p>

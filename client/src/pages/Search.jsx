6b3eeec3354e93d6e5c1795dbc7f3d8f343fb316
Pratik <pratikpattanshetti1007@gmail.com>
2024-02-27 18:21:06 +0530

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -16,8 +16,8 @@ export default function Search() {
 
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-   // const [showMore, setShowMore] = useState(false);
-   console.log(listings);
+    const [showMore, setShowMore] = useState(false);
+   //console.log(listings);
 
       useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -50,15 +50,15 @@ export default function Search() {
         }
         const fetchListings = async () => {
             setLoading(true);
-            //setShowMore(false);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
-            // if (data.length > 8) {
-            //   setShowMore(true);
-            // } else {
-            //   setShowMore(false);
-            // }
+            if (data.length > 8) {
+              setShowMore(true);
+            } else {
+              setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
           };
@@ -114,6 +114,20 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+      };
+
+      const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+          setShowMore(false);
+        }
+        setListings([...listings, ...data]);
       };
 
   return (
@@ -231,6 +245,13 @@ export default function Search() {
                 listings.map((listing) => 
                   <Listingitem key={listing._id} listing={listing} />
                 )}
+                {showMore && (
+                <button
+                  onClick={onShowMoreClick}
+                  className='text-green-700 hover:underline p-7 text-center w-full'>
+                  Show more
+                </button>
+                )}   
             </div>
         </div>
     </div>
